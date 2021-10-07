ca6748233fdf334c8e61eae8aba4d3fafde7e3eb
NoackEric <ericnoack91@gmail.com>
2021-10-07 10:38:50 +0200

    fetch news api

--- index.js
+++ index.js
@@ -14,17 +14,31 @@ const ejs = require('ejs');
 const axios = require('axios');
 
 
+// https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
 
 app.get('/', (req, res) => {
-    res.render('index.ejs', { username: process.env.USERNAME })
+    res.render('index.ejs', { name: process.env.USERNAME })
 })
 app.get('/about', (req, res) => {
     res.render('about.ejs')
 })
 app.get('/contact', (req, res) => {
     res.render('contact.ejs')
+})
+app.get('/news', (req, res) => {
+    axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${process.env.API_KEY}`)
+        .then(function (response) {
+            // handle success
+            console.log(response);
+            res.render('news.ejs', { articles: response.data.articles })
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
 })
 
